71e08f48cd1c2ecb6e74a8dfe5a8ff86b4abd412
haddadsim <haddadsimon@outlook.com>
2019-07-05 16:14:02 +0300

    fix: change let to const in immutable variables

--- learnyounode/filteredLs.js
+++ learnyounode/filteredLs.js
@@ -1,15 +1,13 @@
-let fs = require("fs");
-let path = require("path");
-let fileDirectory = process.argv[2];
-let extensionName = "." + process.argv[3];
+const fs = require("fs");
+const path = require("path");
+const fileDirectory = process.argv[2];
+const extensionName = "." + process.argv[3];
 
 fs.readdir(fileDirectory, (err, list) => {
   list.forEach(file => {
-    
     let ext = path.extname(file);
     if (ext === extensionName) {
       console.log(file);
     }
   });
 });
-
