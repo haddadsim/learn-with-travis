88690c4f8e2578bfa0800881adaeaf565530d624
haddadsim <haddadsimon@outlook.com>
2019-07-16 12:23:33 +0300

    fix: update modal example file

--- src/shared/component/modal-example.jsx
+++ src/shared/component/modal-example.jsx
@@ -14,7 +14,7 @@ const ModalExample = () => (
           This is a Bootstrap modal. It uses jQuery.
         </div>
         <div className="modal-footer">
-          <button type="button" role="button" className="btn btn-primary" data-dismiss="modal">Close</button>
+          <button type="button" className="btn btn-primary" data-dismiss="modal">Close</button>
         </div>
       </div>
     </div>
